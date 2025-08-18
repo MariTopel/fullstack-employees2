d8537e0cd844317c69951c33857428d34f7eea5d
Mari Topel <maritopel@gmail.com>
2025-08-18 18:30:37 -0500

    made major fix to reset password and changed bd:schema thingy to work with my new password work around

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,11 @@
 DROP TABLE IF EXISTS employees;
 
+--create employee table
+CREATE TABLE employees(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    birthday DATE NOT NULL,
+    salary INTERGER NOT NULL
+);
+
 
