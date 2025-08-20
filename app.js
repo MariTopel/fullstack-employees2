30857b4d53b082ec67bbc692197af9247221fdf5
Mari Topel <maritopel@gmail.com>
2025-08-20 11:37:11 -0500

    added app.js coding

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 import express from "express";
+import db from "#db/client";
 const app = express();
 export default app;
 
 // TODO: this file!
+app.get("/employees", async (_req, res, next) => {
+  try {
+    const { rows } = await db.query("SELECT * FROM employees ORDER BY id;");
+    res.status(200).json(rows);
+  } catch (err) {
+    next(err);
+  }
+});
