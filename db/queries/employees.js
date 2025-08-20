bd954ec623f1601fc1f1e4ee14901ef3c421901b
Mari Topel <maritopel@gmail.com>
2025-08-20 11:27:35 -0500

    added the code for all the tests

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,13 +1,22 @@
+import db from "#db/client";
+
 /** @returns the employee created according to the provided details */
 export async function createEmployee({ name, birthday, salary }) {
-  // TODO
+  const sql = `
+    INSERT INTO employee (name, birthday, salary)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+    `; //RETURNING * gives the row back
+  const { rows } = await db.query(sql, [name, birthday, salary]);
+  return rows[0]; //a single row of the new employee
 }
 
 // === Part 2 ===
 
 /** @returns all employees */
 export async function getEmployees() {
-  // TODO
+  const { rows } = await db.query("SELECT * FROM employees ORDER BY id;");
+  return rows;
 }
 
 /**
@@ -15,7 +24,10 @@ export async function getEmployees() {
  * @returns undefined if employee with the given id does not exist
  */
 export async function getEmployee(id) {
-  // TODO
+  const { rows } = await db.query("SELECT * FROM employees WHERE id = $1;", [
+    id,
+  ]);
+  return rows[0];
 }
 
 /**
@@ -23,7 +35,16 @@ export async function getEmployee(id) {
  * @returns undefined if employee with the given id does not exist
  */
 export async function updateEmployee({ id, name, birthday, salary }) {
-  // TODO
+  const sql = `
+  UPDATE employees
+  SET name = $2, birthday = $3, salary = $4
+  WHERE id = $1 
+  RETURNING *;
+  `;
+  //WHERE id = $1 updates one row if it exists
+  //RETURNING * gives the full updated employee back
+  const { rows } = await db.query(sql, [id, name, birthday, salary]);
+  return rows[0];
 }
 
 /**
@@ -31,5 +52,9 @@ export async function updateEmployee({ id, name, birthday, salary }) {
  * @returns undefined if employee with the given id does not exist
  */
 export async function deleteEmployee(id) {
-  // TODO
+  const { rows } = await db.query(
+    "DELETE FROM employees WHERE id = $1 RETURNING *;",
+    [id]
+  );
+  return rows[0];
 }
